cc7cedc4b3c88c24ee6e55c267101e35bffc29ca
Dongkwan Kim <todoaskit@gmail.com>
2018-12-21 11:28:09 +0900

    Add basic prop.py

---
+++ propy/NetworkUtil.py
@@ -0,0 +1,181 @@
+import os
+from typing import Callable, Any, List, Tuple
+
+import networkx as nx
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def get_scale_free_graph(n, seed, force_save=True, **kwargs) -> nx.DiGraph:
+    path = "scale_free_n{}_{}.gpickle".format(n, kwargs)
+    if force_save:
+        g = nx.DiGraph(nx.scale_free_graph(n=n, seed=seed, **kwargs))
+        g.remove_edges_from(g.selfloop_edges())
+        setattr(g, "path", path)
+        nx.write_gpickle(g, path)
+        return g
+    try:
+        g = nx.read_gpickle(os.path.join(".", path))
+        print("Load: {}".format(path))
+    except FileNotFoundError:
+        g = nx.DiGraph(nx.scale_free_graph(n=n, seed=seed, **kwargs))
+        g.remove_edges_from(g.selfloop_edges())
+        setattr(g, "path", path)
+        nx.write_gpickle(g, os.path.join(".", path))
+        print("Dump: {}".format(path))
+    return g
+
+
+def sample_propagation_roots(g: nx.Graph,
+                             num_info: int,
+                             root_selection_metric: Callable = nx.closeness_centrality,
+                             seed: int = None) -> np.ndarray:
+    """
+    :param g: Graph class of Network
+    :param num_info: Number of propagation
+    :param root_selection_metric: default is nx.closeness_centrality
+    :param seed: Indicator of random number generation state.
+    :return:
+    """
+    np.random.seed(seed)
+
+    sorted_node_and_value = sorted(root_selection_metric(g).items(), key=lambda kv: -kv[1])
+    sum_value = sum([v for n, v in sorted_node_and_value])
+    normalized_probs = [float(v) / sum_value for n, v in sorted_node_and_value]
+    chosen_nodes = np.random.choice(
+        [n for n, _ in sorted_node_and_value],
+        size=num_info,
+        p=normalized_probs,
+    )
+    return chosen_nodes
+
+
+def get_propagation_events(g: nx.DiGraph,
+                           root: Any,
+                           propagation_prob: float,
+                           max_iter: int,
+                           decay_rate: float = 1,
+                           seed: int = None) -> List[Tuple]:
+    np.random.seed(seed)
+
+    exposed_and_not_propagated_node_to_parent = dict()
+    propagated_events = list()
+
+    propagated_events.append((1, "ROOT", root))
+    exposed_and_not_propagated_node_to_parent.update({p: root for p in g.predecessors(root) if p != root})
+
+    for t in range(2, max_iter + 2):
+
+        # list(...keys()): To prevent "dictionary changed size during iteration"
+        for node in list(exposed_and_not_propagated_node_to_parent.keys()):
+
+            is_propagated = np.random.choice([True, False], p=[propagation_prob, 1 - propagation_prob])
+            if is_propagated:
+
+                parent = exposed_and_not_propagated_node_to_parent[node]
+                del exposed_and_not_propagated_node_to_parent[node]
+
+                propagated_events.append((t, parent, node))
+
+                exposed_and_not_propagated_node_to_parent.update({
+                    follower: node for follower in g.predecessors(node)
+                    if follower not in [e[-1] for e in propagated_events]
+                })
+
+        if len(propagated_events) == len(g.nodes):
+            break
+
+        propagation_prob *= decay_rate
+
+    return propagated_events
+
+
+# Drawing
+
+def get_highlight_node_color(all_nodes, highlight_nodes, base_color="grey", highlight_color="red"):
+    node_color = []
+    for n in all_nodes:
+        if n in highlight_nodes:
+            node_color.append(highlight_color)
+        else:
+            node_color.append(base_color)
+    return node_color
+
+
+def draw_graph(g: nx.DiGraph, **kwargs):
+    """
+    :param g:
+    :param node_color = get_highlight_node_color(g.nodes, highlight_nodes)
+    :return:
+    """
+    settings = {"node_size": 10, "with_labels": True, "edge_color": "#777777", "width": 0.3, "font_size": 6}
+    settings.update(kwargs)
+    drawing_method = kwargs["drawing_method"] if "drawing_method" in kwargs else nx.draw_kamada_kawai
+
+    copied_g = nx.DiGraph()
+    copied_g.add_nodes_from(g.nodes)
+    copied_g.add_edges_from(nx.get_edge_attributes(g, "follow").keys())
+    drawing_method(copied_g, **settings)
+    plt.show()
+    plt.clf()
+
+
+# Overrides NetworkX methods
+
+def to_numpy_matrix(G, nodelist=None, dtype=None, order=None,
+                    multigraph_weight=sum, weight='weight', nonedge=0.0,
+                    value_for_non_weight_exist=1):
+    A = to_numpy_array(G, nodelist=nodelist, dtype=dtype, order=order,
+                       multigraph_weight=multigraph_weight, weight=weight,
+                       nonedge=nonedge, value_for_non_weight_exist=value_for_non_weight_exist)
+    M = np.asmatrix(A, dtype=dtype)
+    return M
+
+
+def to_numpy_array(G, nodelist=None, dtype=None, order=None,
+                   multigraph_weight=sum, weight='weight', nonedge=0.0,
+                   value_for_non_weight_exist=1):
+
+    if nodelist is None:
+        nodelist = list(G)
+    nodeset = set(nodelist)
+    if len(nodelist) != len(nodeset):
+        msg = "Ambiguous ordering: `nodelist` contained duplicates."
+        raise nx.NetworkXError(msg)
+
+    nlen = len(nodelist)
+    undirected = not G.is_directed()
+    index = dict(zip(nodelist, range(nlen)))
+
+    if G.is_multigraph():
+        # Handle MultiGraphs and MultiDiGraphs
+        A = np.full((nlen, nlen), np.nan, order=order)
+        # use numpy nan-aware operations
+        operator = {sum: np.nansum, min: np.nanmin, max: np.nanmax}
+        try:
+            op = operator[multigraph_weight]
+        except:
+            raise ValueError('multigraph_weight must be sum, min, or max')
+
+        for u, v, attrs in G.edges(data=True):
+            if (u in nodeset) and (v in nodeset):
+                i, j = index[u], index[v]
+                e_weight = attrs.get(weight, value_for_non_weight_exist)
+                A[i, j] = op([e_weight, A[i, j]])
+                if undirected:
+                    A[j, i] = A[i, j]
+    else:
+        # Graph or DiGraph, this is much faster than above
+        A = np.full((nlen, nlen), np.nan, order=order)
+        for u, nbrdict in G.adjacency():
+            for v, d in nbrdict.items():
+                try:
+                    A[index[u], index[v]] = d.get(weight, value_for_non_weight_exist)
+                except KeyError:
+                    # This occurs when there are fewer desired nodes than
+                    # there are nodes in the graph: len(nodelist) < len(G)
+                    pass
+
+    A[np.isnan(A)] = nonedge
+    A = np.asarray(A, dtype=dtype)
+    return A
---
+++ propy/prop.py
@@ -0,0 +1,170 @@
+from collections import defaultdict
+
+import networkx as nx
+import numpy as np
+import propy.NetworkUtil as nu
+
+from typing import List, Tuple, Dict, Sequence, Callable
+from pprint import pprint
+
+
+class NetworkPropagation(nx.DiGraph):
+
+    def __init__(self,
+                 nodes: Sequence,
+                 edges: List[Tuple],
+                 num_info: int,
+                 propagation: Dict[int, List[Tuple]] or float,
+                 user_actions: List[str] = None,
+                 seed: int = 42,
+                 **attr):
+
+        self.seed = seed
+        super().__init__(**attr)
+
+        self.add_nodes_from(nodes)
+        self.add_edges_from(edges, follow=1)
+
+        self.num_info = num_info
+
+        self.user_actions, user_actions = [], user_actions if user_actions else []
+        self.user_actions.append("follow")
+        self._append_user_actions_with_info("propagate")
+        for action_key in user_actions:
+            self._append_user_actions_with_info(action_key)
+
+        self.info_to_propagation: Dict[int, List[Tuple]] = self._get_info_to_propagation(num_info, propagation)
+        self.info_to_attributes: Dict[int, Dict] = {info: {} for info in self.info_to_propagation.keys()}
+        self.event_listeners = defaultdict(list)
+
+    def _append_user_actions_with_info(self, action_key):
+        for info in range(self.num_info):
+            self.user_actions.append("{}_{}".format(action_key, info))
+
+    def _get_info_to_propagation(self,
+                                 num_info: int,
+                                 propagation: Dict[int, List[Tuple]] or float) -> Dict[int, List[Tuple]]:
+
+        propagation_dict = dict()
+
+        # Generate propagation with probability p if propagation is probability (float)
+        if isinstance(propagation, float):
+            roots = nu.sample_propagation_roots(self, num_info, seed=self.seed)
+            for i, root in enumerate(roots):
+                events = nu.get_propagation_events(self, root, propagation, max_iter=len(self.nodes), seed=self.seed)
+                propagation_dict[i] = events
+
+        # Add edges with propagate
+        propagation_dict = propagation_dict or propagation
+        for info, propagation in propagation_dict.items():
+            for t, parent_id, node_id in propagation[1:]:  # Exclude ROOT -> ...
+                self.add_action(parent_id, node_id, f"propagate_{info}", t)
+
+        return propagation_dict
+
+    # Magic Methods
+
+    def __repr__(self):
+        return self.get_title()
+
+    # Data Methods
+
+    def get_action_matrix(self, action_key: str, time_stamp: int or float = None, is_binary_repr=False) -> np.ndarray:
+        assert action_key in self.user_actions
+
+        action_matrix = nu.to_numpy_matrix(self, weight=action_key, value_for_non_weight_exist=0)
+        if time_stamp is not None:
+            action_matrix = np.multiply(action_matrix, action_matrix <= time_stamp)
+
+        if is_binary_repr:
+            return (action_matrix != 0).astype(int)
+        else:
+            return action_matrix
+
+    # Propagation Methods
+
+    def get_last_time_of_propagation(self) -> int or float:
+        last_times = []
+        for _, propagation in self.info_to_propagation.items():
+            last_times.append(propagation[-1][0])
+        return max(last_times)
+
+    def simulate_propagation(self):
+        for info, propagation in self.info_to_propagation.items():
+            for t, parent_id, node_id in propagation:
+                self._run_event_listener("propagate", (t, parent_id, node_id), info=info)
+
+    # Attributes Manipulation Methods
+
+    def add_action(self, u, v, action_key, value):
+        self.add_edge(u, v, **{action_key: value})
+
+    def get_info_attr(self, info, attr=None):
+        if attr is None:
+            return self.info_to_attributes[info]
+        else:
+            return self.info_to_attributes[info][attr]
+
+    def set_info_attr(self, info, attr, val):
+        self.info_to_attributes[info][attr] = val
+
+    def get_edge_of_attr(self, attr):
+        return list(nx.get_edge_attributes(self, attr).keys())
+
+    # Event Listener Methods
+
+    def add_event_listener(self, event_type: str, callback_func: Callable, **kwargs):
+        """
+        :param event_type: str from ["propagate",]
+        :param callback_func: some_func(network_propagation: NetworkPropagation, event: Tuple, info: int, **kwargs)
+        :param kwargs: kwargs for callback_func
+        :return:
+        """
+        self.event_listeners[event_type].append((callback_func, kwargs))
+
+    def _run_event_listener(self, given_event_type: str, event: Tuple, info: int):
+        for callback_func, kwargs in self.event_listeners[given_event_type]:
+            callback_func(
+                network_propagation=self,
+                event=event,
+                info=info,
+                **kwargs
+            )
+
+    # NetworkX Overrides
+    def predecessors(self, n, feature=None):
+        if feature is None:
+            return super().predecessors(n)
+        else:
+            return [p for p, features in self._pred[n].items() if feature in features]
+
+    # Util Methods
+
+    def get_roots(self) -> List[int]:
+        roots = []
+        for info, history in self.info_to_propagation.items():
+            roots.append(history[0][-1])  # (t, p, n)
+        return roots
+
+    def draw_graph(self):
+        roots = self.get_roots()
+        node_color = nu.get_highlight_node_color(self.nodes, roots)
+        nu.draw_graph(self, node_color=node_color)
+
+    def get_title(self):
+        key_attributes = {
+            "num_info": self.num_info,
+            "nodes": self.number_of_nodes(),
+            "edges": self.number_of_edges(),
+            "seed": self.seed,
+        }
+        return "_".join(["{}_{}".format(k, v) for k, v in key_attributes.items()])
+
+    def pprint_propagation(self):
+        pprint(self.info_to_propagation)
+
+
+if __name__ == '__main__':
+    g = nu.get_scale_free_graph(n=10, seed=42)
+    prop = NetworkPropagation(g.nodes, g.edges(), num_info=5, propagation=0.3, seed=42)
+    prop.draw_graph()
