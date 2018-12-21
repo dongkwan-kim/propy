949090022c2b3c2bc5d9bbe7efd8ea8dc305c826
Dongkwan Kim <todoaskit@gmail.com>
2018-12-21 23:24:49 +0900

    Separate tests

--- test/test_prop.py
+++ test/test_prop.py
@@ -5,7 +5,7 @@ from propy.EventListenerExample import randomly_flag
 
 class TestProp(unittest.TestCase):
 
-    def test_prop(self):
+    def test_prop_basic(self):
         g = nu.get_scale_free_graph(n=5, seed=42)
         prop = NetworkPropagation(g.nodes, g.edges(),
                                   user_actions=["flag"], num_info=1, propagation=0.3,
@@ -51,41 +51,36 @@ class TestProp(unittest.TestCase):
                         [0.0, 0.0, 0.0, 0.0, 0.0]])
         ))
 
+    def test_prop_dump_and_load(self):
+        g = nu.get_scale_free_graph(n=20, seed=42)
+        prop = NetworkPropagation(g.nodes, g.edges(),
+                                  user_actions=["flag"], num_info=2, propagation=0.3,
+                                  is_verbose=False, seed=42)
+        prop.add_event_listener(
+            event_type="propagate",
+            callback_func=randomly_flag,
+            flag_prob=0.6,
+            seed=42,
+        )
+        prop.simulate_propagation()
         prop.dump("test")
 
         loaded_prop = NetworkPropagation.load("test")
-
         self.assertTrue(np.array_equal(
             loaded_prop.get_action_matrix("follow"),
-            np.asarray([[0.0, 1.0, 0.0, 0.0, 0.0],
-                        [1.0, 0.0, 1.0, 0.0, 0.0],
-                        [1.0, 0.0, 0.0, 0.0, 0.0],
-                        [1.0, 1.0, 0.0, 0.0, 0.0],
-                        [0.0, 0.0, 1.0, 0.0, 0.0]])
+            prop.get_action_matrix("follow"),
         ))
         self.assertTrue(np.array_equal(
             loaded_prop.get_action_matrix("propagate_0"),
-            np.asarray([[0.0, 0.0, 3.0, 3.0, 0.0],
-                        [0.0, 0.0, 0.0, 0.0, 0.0],
-                        [0.0, 4.0, 0.0, 0.0, 0.0],
-                        [0.0, 0.0, 0.0, 0.0, 0.0],
-                        [0.0, 0.0, 0.0, 0.0, 0.0]])
+            prop.get_action_matrix("propagate_0"),
         ))
         self.assertTrue(np.array_equal(
             loaded_prop.get_action_matrix("flag_0"),
-            np.asarray([[0.0, 1.0, 0.0, 1.0, 0.0],
-                        [0.0, 0.0, 0.0, 0.0, 0.0],
-                        [0.0, 0.0, 0.0, 0.0, 3.0],
-                        [0.0, 0.0, 0.0, 0.0, 0.0],
-                        [0.0, 0.0, 0.0, 0.0, 0.0]])
+            prop.get_action_matrix("flag_0"),
         ))
         self.assertTrue(np.array_equal(
             loaded_prop.get_action_matrix("flag_0", time_stamp=2),
-            np.asarray([[0.0, 1.0, 0.0, 1.0, 0.0],
-                        [0.0, 0.0, 0.0, 0.0, 0.0],
-                        [0.0, 0.0, 0.0, 0.0, 0.0],
-                        [0.0, 0.0, 0.0, 0.0, 0.0],
-                        [0.0, 0.0, 0.0, 0.0, 0.0]])
+            loaded_prop.get_action_matrix("flag_0", time_stamp=2),
         ))
 
 
